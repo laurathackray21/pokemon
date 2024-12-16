f076af1bbced887c81ebf15611937f0637138325
laura thackray <laura.thackray@sero.life>
2024-12-16 11:50:42 +0000

    update node version and remove log

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { POKEMON_PER_PAGE } from "./consts/pokedex";
 
 export default async function Home() {
   const initialPokemon = await getAllPokemonDetails(POKEMON_PER_PAGE, 0);
-  console.log(initialPokemon);
   return (
     <main className="flex min-h-screen flex-col">
       <PokemonList initialPokemon={initialPokemon}></PokemonList>
